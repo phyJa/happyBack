adefc723628cc0db6dc642a8153457cb0216379b
Elias <eli.bueno9@gmail.com>
2020-10-14 16:52:47 -0300

    After making some tests with the parameters, we reorganize the code

--- src/server.ts
+++ src/server.ts
@@ -8,20 +8,11 @@ app.use(express.json());
 // All inside the next HTTP method (in this case, get) is a route
 // Recurso (resource): "/"
 
-app.post(
+app.get(
 
-    "/:abdc", 
+    "/", 
 
     (request, response) => {
-
-        // Query params
-        //console.log(request.query);
-
-        //Route params
-        //console.log(request.params);
-
-        //Body params
-        console.log(request.body);
 
         response.json({ message: "Hello world!" });
 

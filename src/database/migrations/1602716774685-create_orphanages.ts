75bde8c7fafd1f3de4b83eac8edd9287a0e43e98
Elias <eli.bueno9@gmail.com>
2020-10-14 20:53:49 -0300

    Writing the first migration to create a table of the orphanages

--- src/database/migrations/1602716774685-create_orphanages.ts
+++ src/database/migrations/1602716774685-create_orphanages.ts
@@ -1,11 +1,87 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createOrphanages1602716774685 implements MigrationInterface {
 
+    // Make changes (create a table, create a new field, delete a field)
     public async up(queryRunner: QueryRunner): Promise<void> {
+        
+        await queryRunner.createTable(
+            
+            new Table(
+                {
+                    name: "orphanages", // The name of the table
+                    
+                    columns: [
+                        {
+                            name: "id",
+
+                            type: "integer",
+
+                            unsigned: true, // The id column cannot have negative values
+
+                            isPrimary: true,
+
+                            isGenerated: true,
+
+                            generationStrategy: "increment"
+                        },
+
+                        {
+                            name: "name",
+
+                            type: "varchar"
+                        },
+
+                        {
+                            name: "latitude",
+
+                            type: "decimal",
+
+                            scale: 10,
+
+                            precision: 2
+                        },
+
+                        {
+                            name: "longitude",
+
+                            type: "decimal",
+
+                            scale: 10,
+
+                            precision: 2
+                        },
+
+                        {
+                            name: "about",
+
+                            type: "text"
+                        },
+
+                        {
+                            name: "instructions",
+
+                            type: "text"
+                        },
+
+                        {
+                            name: "open_on_weekends",
+
+                            type: "boolean",
+
+                            default: false
+                        }
+                    ]
+                }
+            )
+        );
     }
 
+    // Undo the changes made by the up method
     public async down(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.dropTable("orphanages");
+
     }
 
 }

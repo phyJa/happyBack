e5857c417ba87801e7416a82aa561825f3bd9e34
Elias <eli.bueno9@gmail.com>
2020-10-14 09:34:30 -0300

    Making some tests with the parameters. Note that when  when we try to send a json file through a request, the express does not understand it

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,25 @@ import express from 'express';
 
 const app = express();
 
-app.get(
+// All inside the next HTTP method (in this case, get) is a route
+// Recurso (resource): "/"
 
-    "/", 
+app.post(
+
+    "/:abdc", 
 
     (request, response) => {
 
-        response.send("Hello world!");
+        // Query params
+        //console.log(request.query);
+
+        //Route params
+        console.log(request.params);
+
+        //Body params
+        //console.log(request.body);
+
+        response.json({ message: "Hello world!" });
 
     }
 

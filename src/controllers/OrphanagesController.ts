7bb3814444656e7c2e20f18c215da970978cddbc
Elias <eli.bueno9@gmail.com>
2020-10-15 15:22:57 -0300

    Splitting the code into controller files to organize better the project

---
+++ src/controllers/OrphanagesController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+
+import { getRepository } from "typeorm";
+
+import Orphanage from '../models/Orphanage';
+
+export default {
+    
+    async create(request: Request, response: Response) {
+        
+        const {
+            
+            name, // name: name
+
+            latitude,
+            
+            longitude,
+            
+            about,
+            
+            instructions,
+            
+            opening_hours,
+            
+            open_on_weekends
+        
+        } = request.body;
+
+        const orphanagesRepository = getRepository(Orphanage);
+        
+        const orphanage = orphanagesRepository.create(
+            
+            {
+
+                name,
+
+                latitude,
+                
+                longitude,
+                
+                about,
+                
+                instructions,
+                
+                opening_hours,
+                
+                open_on_weekends
+
+            }
+
+        );
+
+        await orphanagesRepository.save(orphanage);
+
+        // 201: Code which indicates something was
+        // created.
+        return response.status(201).json(orphanage);
+
+    }
+
+};

39d11a0994d29da295785ea22e30ad51480412c9
Elias <eli.bueno9@gmail.com>
2020-10-15 16:15:33 -0300

    Showing a specific orphanage with the show method

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,13 @@ import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
+// Create an orphanage
 routes.post("/orphanages", OrphanagesController.create);
+
+// List all the orphanages
 routes.get("/orphanages", OrphanagesController.index);
+
+// Show a specific orphanage
+routes.get("/orphanages/:id", OrphanagesController.show);
 
 export default routes;

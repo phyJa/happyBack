672a69604e55dfadb658e56b0b466f6db20dcbc7
Elias <eli.bueno9@gmail.com>
2020-10-15 20:46:08 -0300

    Writing more code to the 'upload.ts' file

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -8,9 +8,23 @@ import path from "path";
 export default {
 
     storage: multer.diskStorage(
+
         {
-            destination: path.join(__dirname, "")
+        
+            destination: path.join(__dirname, "..", "..", "uploads"),
+
+            filename: (request, file, cb) => {
+
+                // Changing the fileName to reduce the possibility of two equal names
+                const fileName = `${Date.now()}-${file.originalname}`;
+
+                // As no error is expected, we pass null as the first parameter
+                cb(null, fileName);
+
+            }
+        
         }
+    
     )
 
 };

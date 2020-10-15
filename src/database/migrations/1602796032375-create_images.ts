a24ab3bfd14b8670b8e3aeb1ca8b7c8ac85bcd70
Elias <eli.bueno9@gmail.com>
2020-10-15 19:14:10 -0300

    Migration to create a table for the images is ready

--- src/database/migrations/1602796032375-create_images.ts
+++ src/database/migrations/1602796032375-create_images.ts
@@ -1,11 +1,81 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createImages1602796032375 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+
+        queryRunner.createTable(
+
+            new Table(
+
+                {
+
+                    name: "images",
+
+                    columns: [
+                        {
+
+                            name: "id",
+
+                            type: "integer",
+
+                            unsigned: true, // The id column cannot have negative values
+
+                            isPrimary: true,
+
+                            isGenerated: true,
+
+                            generationStrategy: "increment"
+
+                        },
+
+                        {
+
+                            name: "path",
+
+                            type: "varchar"
+
+                        },
+
+                        {
+                            
+                            name: "orphanage_id",
+
+                            type: "integer"
+
+                        }
+                    ],
+
+                    foreignKeys: [
+                        {
+                            
+                            name: "imageOrphanage",
+
+                            columnNames: ["orphanage_id"], // The column name of the current table
+
+                            referencedTableName: "orphanages",
+
+                            referencedColumnNames: ["id"],
+
+                            onUpdate: "CASCADE", // If the orphanage id is updated, the images information are kept safe, i.e. their foreign key also change.
+
+                            onDelete: "CASCADE" // If the orphanage is deleted from the database, so are the images.
+
+                        }
+                    ]
+
+                }
+
+            )
+        
+        );
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.dropTable("images");
+
     }
 
 }

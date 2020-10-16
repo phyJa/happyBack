9e883c5232516488d3e001adf131cfe5acd0cbc6
Elias <eli.bueno9@gmail.com>
2020-10-16 11:25:26 -0300

    Accessing images through the API: done

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import './database/connection';
 
 import routes from './routes';
 
+import path from 'path';
+
 const app = express();
 
 //Make express understand json
@@ -13,5 +15,7 @@ app.use(express.json());
 // Use routes (must be after the latter command)
 app.use(routes);
 
+//The configuration to serve the images
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 
 app.listen(3333);

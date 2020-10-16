9e0702c08674a766260067a363aef4a3bac9a764
Elias <eli.bueno9@gmail.com>
2020-10-15 21:01:44 -0300

    Configuring routes to make the upload work

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+
 import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
+const upload = multer(uploadConfig);
+
 // Create an orphanage
-routes.post("/orphanages", OrphanagesController.create);
+routes.post("/orphanages", upload.array("images"), OrphanagesController.create);
 
 // List all the orphanages
 routes.get("/orphanages", OrphanagesController.index);
